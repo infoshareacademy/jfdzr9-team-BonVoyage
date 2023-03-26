24a3da5d8e42197b1388daa72653ab553aa53754
Paulina Filipiak <slodzinska.paulina@gmail.com>
2023-03-26 13:10:37 +0200

    added some styling to form

--- src/ui/form/form.styled.ts
+++ src/ui/form/form.styled.ts
@@ -3,10 +3,8 @@ import styled from "styled-components";
 //na razie ogólne stylowanie
 
 export const StyledForm = styled.form`
-  max-width: 640px;
-  width: 100%;
   display: flex;
-  gap: 10rem;
+  gap: 2rem;
   flex-direction: column;
   justify-content: center;
   align-items: center;
