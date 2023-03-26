f9ad258e8afe6b9bd8c08baa9b7ec7a77c000fc3
Paulina Filipiak <slodzinska.paulina@gmail.com>
2023-03-26 12:03:54 +0200

    created form for registering new user account

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,11 +1,25 @@
+import { useForm, SubmitHandler } from "react-hook-form";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 import { Button } from "../ui/button/button.styled";
 import { StyledForm } from "../ui/form/form.styled";
+import { auth } from "../firebase/firebase.config";
 
+interface IFormInput {
+  email: string;
+  password: string;
+}
 const RegisterPage = () => {
+  const { register, handleSubmit } = useForm<IFormInput>();
+  const onSubmit: SubmitHandler<IFormInput> = ({ email, password }) => {
+    createUserWithEmailAndPassword(auth, email, password);
+  };
+
   return (
     <>
-      <StyledForm>
+      <StyledForm onSubmit={handleSubmit(onSubmit)}>
         <h1>Register</h1>
+        <input {...register("email")} />
+        <input {...register("password")} />
         <Button type="submit">Register</Button>
       </StyledForm>
     </>
