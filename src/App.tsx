76fbcf3a530ee035d5dacb33bfdf00017929d8c7
Paulina Filipiak <slodzinska.paulina@gmail.com>
2023-03-26 12:01:28 +0200

    added registerPage to the main routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { GlobalStyles } from "./GlobalStyles/GlobalStyles";
 import { createBrowserRouter, Route, RouterProvider, createRoutesFromElements } from "react-router-dom";
 import { LandingPage } from "./pages/LandingPage";
+import RegisterPage from "./pages/RegisterPage";
 
 const App: React.FC = () => {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<LandingPage />} />
+        <Route path="/login" element={<RegisterPage />} />
       </>,
     ),
   );
