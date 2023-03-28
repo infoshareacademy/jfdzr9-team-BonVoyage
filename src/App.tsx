0320734feeb23c2cdc78ad8814a0fcafc26a8d2e
Paulina Filipiak <slodzinska.paulina@gmail.com>
2023-03-28 21:05:18 +0200

    added addTrip to routing

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ const App: React.FC = () => {
       <>
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<RegisterPage />} />
+        <Route path="/addTrip" element={<AddTrip />} />
       </>,
     ),
   );
