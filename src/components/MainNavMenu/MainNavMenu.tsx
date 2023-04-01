0fa3fc0cb25b1388aeb66c01878316efc3b85a39
Paulina Filipiak <slodzinska.paulina@gmail.com>
2023-04-01 21:28:06 +0200

    deleted unused variables in MainNavMenu

--- src/components/MainNavMenu/MainNavMenu.tsx
+++ src/components/MainNavMenu/MainNavMenu.tsx
@@ -1,4 +1,4 @@
-import { NavMenuContainer, NavMenuItem, NavMenuDiv } from "./MainNavMenu.styled";
+import { NavMenuContainer, NavMenuDiv } from "./MainNavMenu.styled";
 import { Link, NavLink } from "react-router-dom";
 import { useState } from "react";
 import { useMediaQuery } from "../../hooks/UseMediaQuery";
@@ -9,16 +9,7 @@ interface NavigationProps {
   something?: string;
 }
 
-interface MenuItem {
-  title: string;
-  url: string;
-}
-
-interface Props {
-  menuItems: MenuItem[];
-}
-
-export const MainNavMenu: React.FC<NavigationProps> = (props): JSX.Element => {
+export const MainNavMenu: React.FC<NavigationProps> = (): JSX.Element => {
   const bigScreen = useMediaQuery("(min-width: 768px)");
   const [open, setOpen] = useState(false);
   if (bigScreen) {
