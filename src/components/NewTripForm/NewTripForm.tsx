654b25de129bf00a8ed73b535fa04d978d013012
Dawid Matras <davematras@gmail.com>
2023-04-05 23:06:26 +0200

    basic creating new trip in firebase completed + adding picture to storage

--- src/components/NewTripForm/NewTripForm.tsx
+++ src/components/NewTripForm/NewTripForm.tsx
@@ -3,20 +3,29 @@ import { Button } from "../../ui/button/button.styled";
 import { StyledFormNewTrip } from "../../ui/form/form.styled";
 import { TextInput } from "../../ui/TextInput/TextInput.styled";
 import { useForm } from "react-hook-form";
-import { db, auth } from "../../firebase/firebase.config";
+import { db, auth, storage } from "../../firebase/firebase.config";
 import { doc, setDoc } from "firebase/firestore";
+import { ref, uploadBytes } from "firebase/storage";
 
 type FormData = {
   title: string;
   description: string;
-  imageUrl: string[];
+  imageUrl: FileList;
 };
 const NewTripForm = () => {
   const { register, handleSubmit } = useForm<FormData>();
   const navigate = useNavigate();
   const onSubmit = handleSubmit(({ title, description, imageUrl }) => {
-    const id = `${title}-${Math.floor(Math.random() * 1000000 + 1)}`;
-    const trip = { title, description, imageUrl, userEmail: auth.currentUser?.email, places: [] };
+    const id = `${title}-${Math.floor(Math.random() * 100000000 + 1)}`;
+    const imageRef = ref(storage, `${auth.currentUser?.email}/${id}`);
+    uploadBytes(imageRef, imageUrl[0]);
+    const trip = {
+      title,
+      description,
+      imageUrl: imageRef.fullPath,
+      userEmail: auth.currentUser?.email,
+      places: [],
+    };
     const tripRef = doc(db, "trips", id);
     setDoc(tripRef, trip);
     navigate(`/voyages/${id}`);
@@ -27,7 +36,7 @@ const NewTripForm = () => {
       <h1>Create new trip</h1>
       <TextInput placeholder="Trip Title" {...register("title")} required />
       <TextInput placeholder="Description" {...register("description")} required />
-      <TextInput type="text" {...register("imageUrl")} />
+      <TextInput type="file" {...register("imageUrl")} />
       <Button>Create and go to next step</Button>
     </StyledFormNewTrip>
   );
