2b00f318690cd3d934b493f785ca6be79678d0a3
Paulina Filipiak <slodzinska.paulina@gmail.com>
2023-04-12 10:48:50 +0200

    added function that uploads files

--- src/pages/AccountPage.tsx
+++ src/pages/AccountPage.tsx
@@ -6,6 +6,8 @@ import { StyledForm } from "../ui/form/form.styled";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { Button } from "../ui/button/button.styled";
 import UsersDetails from "../components/UsersDetails/UsersDetails";
+import { ref, uploadBytes, getDownloadURL, listAll } from "firebase/storage";
+import { storage } from "../firebase/firebase.config";
 
 export interface UsersDetailsFormInput {
   firstName: string;
@@ -18,8 +20,22 @@ export interface UsersDetailsFormInput {
 const AccountPage = () => {
   const { register, handleSubmit } = useForm<UsersDetailsFormInput>();
   const [success, setSuccess] = useState(false);
+  const [imgUrl, setImgUrl] = useState("");
+  const [file, setFile] = useState<File | null>(null);
 
   const user = useUser();
+
+  const imagesRef = ref(storage, "users-avatar/");
+
+  const uploadImage = () => {
+    if (!file) return;
+    const imageRef = ref(storage, `users-avatar/${file.name}`);
+    uploadBytes(imageRef, file).then((snapshot) => {
+      getDownloadURL(snapshot.ref).then((url) => {
+        setImgUrl(url);
+      });
+    });
+  };
 
   const onSubmit: SubmitHandler<UsersDetailsFormInput> = (data) => {
     if (user) {
@@ -38,10 +54,11 @@ const AccountPage = () => {
           {" "}
           <h1>Hello {user.email} please fill in this form:</h1>
           <StyledForm onSubmit={handleSubmit(onSubmit)}>
+            <input type="file"></input>
             <TextInput placeholder="First name" type={"text"} {...register("firstName")} required />
             <TextInput placeholder="Last name" type={"text"} {...register("lastName")} required />
             <TextInput placeholder="City" type={"text"} {...register("city")} required />
-            <TextInput alt="Uppload photos" type={"image"} {...register("imageUrl")} />
+            <TextInput alt="Uppload photos" type={"hidden"} {...register("imageUrl")} />
             <Button type="submit">Submit</Button>
           </StyledForm>{" "}
         </>
