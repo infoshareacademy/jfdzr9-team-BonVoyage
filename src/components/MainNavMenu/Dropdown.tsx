397d24b75f5871b3307c29a0cedd0575a7bec071
Tomek <smuga2505tomek@gmail.com>
2023-04-13 23:21:21 +0200

    Finshed Dropdown

--- src/components/MainNavMenu/Dropdown.tsx
+++ src/components/MainNavMenu/Dropdown.tsx
@@ -1,30 +1,66 @@
 import { ProfileSVG } from "../../assets/ProfileSVG";
 import { useState } from "react";
-import { DropdownButton, DropdownContainer, DropdownListItem, DropdownList } from "./Dropdown.style";
+import { DropdownButton, DropdownContainer, DropdownListItem, DropdownList, LinkStyle } from "./Dropdown.style";
 import { Link } from "react-router-dom";
+import { useUser } from "../../context/auth.context";
+import AccountPage from "../../pages/AccountPage";
 
 export function Dropdown() {
+  const user = useUser();
   const [isOpen, setIsOpen] = useState(false);
-  const options = ["Option 2", "Option 3"];
+  const options = [
+    {
+      label: "Sign in",
+      path: "/signIn",
+    },
+    {
+      label: "Register",
+      path: "signIn/register",
+    },
+  ];
 
+  const loggedInOptions = [{ label: "Your Profile", path: "/account" }];
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
 
   return (
-    <DropdownContainer>
-      <DropdownButton onClick={toggleMenu}>
-        <ProfileSVG />
-      </DropdownButton>
-      {isOpen && (
-        <DropdownList>
-          {options.map((option) => (
-            <DropdownListItem key={option}>
-              <p>{option}</p>
-            </DropdownListItem>
-          ))}
-        </DropdownList>
+    <>
+      {user ? (
+        <DropdownContainer>
+          <DropdownButton onClick={toggleMenu}>
+            <ProfileSVG />
+          </DropdownButton>
+          {isOpen && (
+            <DropdownList>
+              {loggedInOptions.map((option) => (
+                <DropdownListItem key={option.path}>
+                  <LinkStyle to={option.path}>
+                    <p>{option.label}</p>
+                  </LinkStyle>
+                </DropdownListItem>
+              ))}
+            </DropdownList>
+          )}
+        </DropdownContainer>
+      ) : (
+        <DropdownContainer>
+          <DropdownButton onClick={toggleMenu}>
+            <ProfileSVG />
+          </DropdownButton>
+          {isOpen && (
+            <DropdownList>
+              {options.map((option) => (
+                <DropdownListItem key={option.path}>
+                  <LinkStyle to={option.path}>
+                    <p>{option.label}</p>
+                  </LinkStyle>
+                </DropdownListItem>
+              ))}
+            </DropdownList>
+          )}
+        </DropdownContainer>
       )}
-    </DropdownContainer>
+    </>
   );
 }
