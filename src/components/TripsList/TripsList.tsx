f4c7bf172469541b2bd57c5262232f4b5cbc30f5
Paulina Filipiak <slodzinska.paulina@gmail.com>
2023-04-14 20:37:28 +0200

    logged to the console all trips  from firebase

--- src/components/TripsList/TripsList.tsx
+++ src/components/TripsList/TripsList.tsx
@@ -1,29 +1,36 @@
 import { TripsListStyled } from "./TripsList.styled";
 import { SingleTrip } from "./SingleTrip";
-import getTrip from "../../firebase/getTrip";
+import getTrips from "../../firebase/getTrip";
 
 const trips = [
   {
-    name: "Warsaw",
-    url: "https://firebasestorage.googleapis.com/v0/b/bonvoyage-e7ad8.appspot.com/o/smuga2505tomek%40gmail.com%2FTitle-17137804%2FBlixter-2048x1451.jpg?alt=media&token=ebd440fc-0ef2-47db-b1af-045e1489f7a8",
+    title: "Warsaw",
+    imageUrl:
+      "https://firebasestorage.googleapis.com/v0/b/bonvoyage-e7ad8.appspot.com/o/smuga2505tomek%40gmail.com%2FTitle-17137804%2FBlixter-2048x1451.jpg?alt=media&token=ebd440fc-0ef2-47db-b1af-045e1489f7a8",
   },
   {
-    name: "Warsaw1",
-    url: "https://firebasestorage.googleapis.com/v0/b/bonvoyage-e7ad8.appspot.com/o/raz%40wp.pl%2FCos-54190152%2Fwinnica-kinga-1.jpg?alt=media&token=2117c2ca-7f2f-45f9-94af-64819a9b637d",
+    title: "Warsaw1",
+    imageUrl:
+      "https://firebasestorage.googleapis.com/v0/b/bonvoyage-e7ad8.appspot.com/o/raz%40wp.pl%2FCos-54190152%2Fwinnica-kinga-1.jpg?alt=media&token=2117c2ca-7f2f-45f9-94af-64819a9b637d",
   },
   {
-    name: "Warsaw2",
-    url: "https://firebasestorage.googleapis.com/v0/b/bonvoyage-e7ad8.appspot.com/o/raz%40wp.pl%2FZG-4086729%2Fwinobranie.jpg?alt=media&token=ef6c4dc9-a30a-4c9f-a137-8b0934b4469a",
+    title: "Warsaw2",
+    imageUrl:
+      "https://firebasestorage.googleapis.com/v0/b/bonvoyage-e7ad8.appspot.com/o/raz%40wp.pl%2FZG-4086729%2Fwinobranie.jpg?alt=media&token=ef6c4dc9-a30a-4c9f-a137-8b0934b4469a",
   },
   {
-    name: "Warsaw3",
-    url: "https://firebasestorage.googleapis.com/v0/b/bonvoyage-e7ad8.appspot.com/o/raz%40wp.pl%2Fa-15679948%2Fwino.jpg?alt=media&token=18fe58d0-ba15-4c04-8ba1-d8eabfe65a3a",
+    title: "Warsaw3",
+    imageUrl:
+      "https://firebasestorage.googleapis.com/v0/b/bonvoyage-e7ad8.appspot.com/o/raz%40wp.pl%2Fa-15679948%2Fwino.jpg?alt=media&token=18fe58d0-ba15-4c04-8ba1-d8eabfe65a3a",
   },
   {
-    name: "Warsaw4",
-    url: "https://firebasestorage.googleapis.com/v0/b/bonvoyage-e7ad8.appspot.com/o/users-avatar%2Favatar.jpg?alt=media&token=da740dbc-b0d9-40bc-8024-6415d43d5c00",
+    title: "Warsaw4",
+    imageUrl:
+      "https://firebasestorage.googleapis.com/v0/b/bonvoyage-e7ad8.appspot.com/o/users-avatar%2Favatar.jpg?alt=media&token=da740dbc-b0d9-40bc-8024-6415d43d5c00",
   },
 ];
+
+getTrips();
 
 // const trips = getTrip();
 
@@ -31,7 +38,7 @@ export const TripsList = () => {
   return (
     <TripsListStyled>
       {trips.map((trip) => (
-        <SingleTrip key={trip.name} url={trip.url} />
+        <SingleTrip key={trip.title} url={trip.imageUrl} />
       ))}
     </TripsListStyled>
   );
--- src/firebase/getTrip.tsx
+++ src/firebase/getTrip.tsx
@@ -1,20 +1,35 @@
-import { DocumentReference, doc, getDoc } from "firebase/firestore";
-import { db } from "./firebase.config";
+import { DocumentReference, collection, doc, getDoc, getDocs } from "firebase/firestore";
+import { db, auth } from "./firebase.config";
 import { useState } from "react";
+import { Trip } from "../pages/AddTrip";
+import { useUser } from "../context/auth.context";
 
-const getTrip = async (uid: string): Promise<User | null> => {
+const getTrips = async () => {
   // const [doc, setDoc] = useState({});
   // const tripRef = doc(db, "trips", uid) as DocumentReference<User>;
   // const res = await getDoc(userRef);
   // const usersDetails = res.data() as User | null;
-  const tripsData = firebase
-    .firestore()
-    .collection("trips")
-    .where("userEmail", "=", firebase.auth().currentUser.email)
-    .get()
-    .then((snapshot) => {
-      snapshot.forEach((doc) => console.log("doc is ", doc));
-    });
+  // const user = useUser();
+
+  // const tripsData = firebase
+  //   .firestore()
+  //   .collection("trips")
+  //   .where("userEmail", "=", firebase.auth().currentUser.email)
+  //   .get()
+  //   .then((snapshot) => {
+  //     snapshot.forEach((doc) => console.log("doc is ", doc));
+  //   });
+  const tripRef = collection(db, "trips");
+  const docsSnap = await getDocs(tripRef);
+  // const data = docsSnap.data();
+  // const filteredData = data.filter((trip) => trip.userEmail === auth.currentUser?.email);
+  // console.log(filteredData);
+  // return filteredData;
+  docsSnap.forEach((doc) => {
+    console.log(doc.data());
+  });
+  // console.log(docsSnap.data());
+  // return docsSnap.data();
 };
 
-export default getTrip;
+export default getTrips;
