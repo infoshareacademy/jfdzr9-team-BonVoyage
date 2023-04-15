998e6274e5e2caddd9f1beb105fd63867e4bd605
Paulina Filipiak <slodzinska.paulina@gmail.com>
2023-04-15 11:55:28 +0200

    deleted error

--- src/firebase/getTrip.tsx
+++ src/firebase/getTrip.tsx
@@ -3,8 +3,6 @@ import { db } from "./firebase.config";
 
 import { Trip } from "../pages/AddTrip";
 
-import firebase from "firebase";
-
 // const getTrips = async (): Promise<Trip[] | null> => {
 //   const tripRef = collection(db, "trips") as firebase.firestore.QuerySnapshot<Trip>;
 //   const docsSnap = await getDocs(tripRef);
@@ -15,11 +13,10 @@ import firebase from "firebase";
 // };
 
 const getTrips = async (): Promise<Trip[] | null> => {
-  const tripRef = collection(db, "trips") as firebase.firestore.QuerySnapshot<Trip>;
+  const tripRef = collection(db, "trips");
   const docsSnap = await getDocs(tripRef);
   const data = docsSnap.docs.map((doc) => doc.data()) as Trip[];
 
-  // console.log(data);
   return data;
 };
 
