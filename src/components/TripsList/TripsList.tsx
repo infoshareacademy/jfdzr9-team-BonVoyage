04ab8c56603dfd247e4cead7f66626dc11098ba9
Paulina Filipiak <slodzinska.paulina@gmail.com>
2023-04-16 12:58:16 +0200

    removed error

--- src/components/TripsList/TripsList.tsx
+++ src/components/TripsList/TripsList.tsx
@@ -1,7 +1,12 @@
 import { TripsListStyled } from "./TripsList.styled";
 import { SingleTrip } from "./SingleTrip";
+import { Trip } from "../../pages/AddTrip";
 
-export const TripsList = ({ trips }) => {
+export interface TripsListProps {
+  trips: Trip[];
+}
+
+export const TripsList = ({ trips }: TripsListProps) => {
   return (
     <TripsListStyled>
       {trips.map((trip) => (
