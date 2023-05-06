256787708cd10a06dc37a4272e796ad1e7d9ebd9
Tomek <smuga2505tomek@gmail.com>
2023-05-06 10:06:00 +0200

    made the carousel work

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,20 +1,43 @@
 // @ts-nocheck
 
+// import { TestVoyageData } from "./TestVoyageData";
+// import { StyledLink } from "./Carousel.style";
+// import { useMediaQuery } from "../../hooks/UseMediaQuery";
+// import "./Swiper.css";
+// import { register } from "swiper/element/bundle";
+// import { useEffect, useRef } from "react";
+// register();
+// declare module "react" {
+//   interface HTMLAttributes<T> {
+//     navigation?: string;
+//     pagination?: string;
+//   }
+// }
+
+//export const Carousel: React.FC = () => {
+//   useEffect(() => {
+//     const swiperContainer = swiperRef.current;
+//     const params = {
+//       navigation: true,
+//       pagination: {
+//         clickable: true,
+//       },
+//       loop: true,
+//       slidesPerView: smallScreen ? 1 : 3,
+
+//     Object.assign(swiperContainer, params);
+//     swiperContainer.initialize();
+//   }, [smallScreen]);
+// };
+
+import { useEffect, useRef } from "react";
+import { register } from "swiper/element/bundle";
+register();
+import { useMediaQuery } from "../../hooks/UseMediaQuery";
 import { TestVoyageData } from "./TestVoyageData";
 import { StyledLink } from "./Carousel.style";
-import { useMediaQuery } from "../../hooks/UseMediaQuery";
-import "./Swiper.css";
-import { register } from "swiper/element/bundle";
-import { useEffect, useRef } from "react";
-register();
-declare module "react" {
-  interface HTMLAttributes<T> {
-    navigation?: string;
-    pagination?: string;
-  }
-}
 
-export const Carousel: React.FC = () => {
+export const Carousel = () => {
   const swiperRef = useRef(null);
   const smallScreen = useMediaQuery("(max-width: 768px)");
 
@@ -22,9 +45,7 @@ export const Carousel: React.FC = () => {
     const swiperContainer = swiperRef.current;
     const params = {
       navigation: true,
-      pagination: {
-        clickable: true,
-      },
+      pagination: true,
       loop: true,
       slidesPerView: smallScreen ? 1 : 3,
       injectStyles: [
@@ -33,7 +54,7 @@ export const Carousel: React.FC = () => {
         .swiper-button-prev {
           color: #d7d7d7;
           transition: all 0.2s ease-in;
-          
+
         }
         .swiper-button-next:hover,
         .swiper-button-prev:hover {
@@ -49,7 +70,7 @@ export const Carousel: React.FC = () => {
         .swiper-container {
           width: 100%;
           height: 50%;
-         
+
           border-radius: 5px;
           padding-bottom: 20px;
         }
@@ -59,16 +80,16 @@ export const Carousel: React.FC = () => {
           background: #fff;
           padding: 10px;
           border-radius: 4px;
-        
+
           display: flex;
           justify-content: center;
           align-items: center;
         }
-        
+
         .swiper-slide:hover {
           transform: scale(1.1);
         }
-        
+
         .swiper-slide img {
           display: block;
           width: 100%;
@@ -76,7 +97,7 @@ export const Carousel: React.FC = () => {
           object-fit: cover;
           padding-bottom: 10px;
         }
-        
+
         .swiper-slide h3 {
           padding-bottom: 20px;
         }
@@ -86,14 +107,14 @@ export const Carousel: React.FC = () => {
 
     Object.assign(swiperContainer, params);
     swiperContainer.initialize();
-  }, [smallScreen]);
+  }, []);
 
   return (
     <>
       <div>
         <h1>Our Voyages</h1>
       </div>
-      <swiper-container class="swiper-container" ref={swiperRef}>
+      <swiper-container ref={swiperRef} init="false">
         {TestVoyageData.map((voyage) => (
           <swiper-slide class="swiper-slide" key={voyage.id}>
             <div className="Card">
@@ -113,3 +134,28 @@ export const Carousel: React.FC = () => {
     </>
   );
 };
+
+{
+  /* <>
+<div>
+  <h1>Our Voyages</h1>
+</div>
+<swiper-container class="swiper-container" ref={swiperRef}>
+  {TestVoyageData.map((voyage) => (
+    <swiper-slide class="swiper-slide" key={voyage.id}>
+      <div className="Card">
+        <div>
+          <img src={voyage.image} alt="" />
+        </div>
+        <div>
+          <StyledLink to={`/voyages/notloggedin/${voyage.id}`}>
+            <h2>{voyage.name}</h2>
+            <h3> {voyage.destination} </h3>
+          </StyledLink>
+        </div>
+      </div>
+    </swiper-slide>
+  ))}
+</swiper-container>
+</> */
+}
